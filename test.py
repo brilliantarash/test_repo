bebd5bf8b5f0216ee42cfd430b93a2f7d4c7b8ef
Arash <arash77.kad@gmail.com>
2024-10-22 15:42:45 +0200

    Refactor workflow to add checkout step and improve secrets handling

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+
+# Get the environment variable
+cmems_password = os.getenv('CMEMS_PASSWORD', 'default-bjoern')
+
+if cmems_password is None:
+    print("CMEMS_PASSWORD is not set!")
+else:
+    print("CMEMS_PASSWORD retrieved successfully.", cmems_password)
