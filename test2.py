edb9c4e51a6c08e4b3933d67a0b2cbc7289d4605
Arash <arash77.kad@gmail.com>
2024-11-06 16:57:14 +0100

    Add error handling for PR_NUMBER environment variable in test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,10 @@ import os
 
 closed = os.getenv("ACTION_TYPE") == "closed"
 merged = os.getenv("MERGED") == "true"
-pr = os.getenv("PR_NUMBER")
+try:
+    pr = int(os.getenv("PR_NUMBER"))
+except:
+    raise Exception("PR_NUMBER is not a number")
 print(f"Closed: {closed}")
 print(f"Merged: {merged}")
 print(f"PR: {pr}")
