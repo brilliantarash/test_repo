67841eda39b9e99f48dc086b9a2ce65500d942df
Arash <arash77.kad@gmail.com>
2024-11-06 16:27:36 +0100

    Add check for closed pull requests and enhance logging in workflow

--- test2.py
+++ test2.py
@@ -2,6 +2,9 @@ import os
 
 action_type = os.getenv("ACTION_TYPE")
 event_name = os.getenv("EVENT_NAME")
+print(f"Action type: {action_type}")
+print(f"Event name: {event_name}")
+print(f"Merged: {os.getenv("MERGED")}")
 if action_type == "closed" and os.getenv("MERGED") == "true":
     print("Running in production mode")
 elif (
