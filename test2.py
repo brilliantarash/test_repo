30ff7c7d0ff465df93e97a0f4481577e1e2021f1
Arash <arash77.kad@gmail.com>
2024-11-06 18:30:39 +0100

    Add GITHUB_TOKEN environment variable to workflow for test2.py execution

--- test2.py
+++ test2.py
@@ -2,7 +2,6 @@ import json
 import os
 
 import github
-import requests
 
 
 with open(os.getenv("GITHUB_EVENT_PATH"), "r") as f:
@@ -15,35 +14,31 @@ print(f"pr_number: {pr_number}")
 print(f"closed: {closed}")
 print(f"pr_merged: {pr_merged}")
 
-token = os.getenv("GITHUB_TOKEN")
-g = github.Github(token)
-repo_name = os.getenv("GITHUB_REPOSITORY")
-repo = g.get_repo(repo_name)
+
+g = github.Github(os.getenv("GITHUB_TOKEN"))
+repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
 pr = repo.get_pull(pr_number)
 
 processed_files_path = "processed_files.json"
 processed_files_branch = "processed_files"
 
-url = f"https://api.github.com/repos/{repo_name}/git/refs"
-headers = {
-    "Authorization": f"token {token}",
-    "Accept": "application/vnd.github.v3+json",
-}
-data = {
-    "ref": f"refs/heads/{processed_files_branch}",
-    "sha": repo.get_branch("main").commit.sha,
-}
-response = requests.post(url, headers=headers, json=data)
-response.raise_for_status()
-message = f"Initialize {processed_files_branch} with {processed_files_path}"
-repo.create_file(
-    path=processed_files_path,
-    message=message,
-    content="{}",
-    branch=processed_files_branch,
-)
-print(message)
-branch = repo.get_branch(processed_files_branch)
+
+try:
+    branch = repo.get_branch(processed_files_branch)
+except:
+    repo.create_git_ref(
+        ref=f"refs/heads/{processed_files_branch}",
+        sha=repo.get_branch("main").commit.sha,
+    )
+    message = f"Initialize {processed_files_branch} with {processed_files_path}"
+    repo.create_file(
+        path=processed_files_path,
+        message=message,
+        content="{}",
+        branch=processed_files_branch,
+    )
+    print(message)
+    branch = repo.get_branch(processed_files_branch)
 
 file_content = repo.get_contents(processed_files_path, ref=branch.commit.sha)
 processed_files = file_content.decoded_content.decode()
