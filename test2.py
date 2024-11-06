107d28ed0a1988b0d72862f6eea397cef4a1f413
Arash <arash77.kad@gmail.com>
2024-11-06 16:14:45 +0100

    Update GitHub Actions workflow to handle pull request events and add Python script for action processing

---
+++ test2.py
@@ -0,0 +1,14 @@
+import os
+
+action_type = os.getenv("ACTION_TYPE")
+event_name = os.getenv("EVENT_NAME")
+if action_type == "closed" and os.getenv("MERGED") == "true":
+    print("Running in production mode")
+elif (
+    event_name == "pull_request_target"
+    and action_type in ["opened", "synchronize", "reopened"]
+) or event_name == "workflow_dispatch":
+    print("Running in preview mode")
+else:
+    print("No action to take")
+print(f"PR: {os.getenv('PR_NUMBER')}")
